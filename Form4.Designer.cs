57869737efa8ee4a8d5ebf73a0ce74e61ee9dc4f
Michael Griffiths <Michael Griffiths>
2016-02-21 19:45:00 +0000

    bright blue to last form

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -141,7 +141,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackColor = System.Drawing.SystemColors.Highlight;
             this.ClientSize = new System.Drawing.Size(453, 370);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
